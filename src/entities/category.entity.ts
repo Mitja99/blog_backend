71197092cc5d0a4a3c98bf67a170da39cdc0346e
mitja stramec <micho.stramec@gmail.com>
2023-12-28 22:06:46 +0100

    prvi commit

---
+++ src/entities/category.entity.ts
@@ -0,0 +1,30 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Blog } from './blog.entity';
+
+@Entity('categories')
+export class Category {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @OneToMany(() => Blog, (blog: Blog) => blog.category)
+  blogs: Blog[];
+}
---
+++ src/user/user.controller.ts
@@ -0,0 +1,52 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post, UseGuards,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { User } from '../entities/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+import { DeleteResult } from 'typeorm';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { JwtAuthGuard } from '../auth/guards/jwtAuth.guard';
+@UseGuards(JwtAuthGuard)
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  async findAll(): Promise<User[]> {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  async findById(@Param('id') id: number): Promise<User> {
+    return this.userService.findById(id);
+  }
+  @Get(':email')
+  async findByEmail(@Param('email') email: string): Promise<User> {
+    return this.userService.findByEmail(email);
+  }
+
+  @Post()
+  async create(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return this.userService.create(createUserDto);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: number): Promise<DeleteResult> {
+    return this.userService.delete(id);
+  }
+
+  @Patch(':id')
+  async update(
+    @Param('id') id: number,
+    @Body() updateUserDto: UpdateUserDto,
+  ): Promise<User> {
+    return this.userService.update(id, updateUserDto);
+  }
+}
